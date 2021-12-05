4ed41a0cdc990c477ad22baa21a198ec7c40b78c
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-05 11:15:31 -0500

    Implementing BEM in Home section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,13 +8,31 @@ const Home = () => {
       <Header />
       <main>
         <div class="main-container">
-          <h1>TOMA CONTROL DE TUS GASTOS HORMIGAS</h1>
-          <h4>Aprende a realizar control y planificación de tus finanzas</h4>
-          <h6>Lleva segui miento de cada peso con reporte detalaldos</h6>
-          <form action="" class="row">
-            <input type="text" name="email" placeholder="Email" />
-            <input type="password" name="password" placeholder="Password" />
-            <button type="submit">INGRESAR</button>
+          <h1 class="main-container__title">
+            TOMA CONTROL DE TUS GASTOS HORMIGAS
+          </h1>
+          <h4 class="main-container__subtitle">
+            Aprende a realizar control y planificación de tus finanzas
+          </h4>
+          <h6 class="main-container__phrase">
+            Lleva segui miento de cada peso con reporte detalaldos
+          </h6>
+          <form class="form" action="">
+            <input
+              class="form__input"
+              type="text"
+              name="email"
+              placeholder="Email"
+            />
+            <input
+              class="form__input"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+            <button class="form__button" type="submit">
+              INGRESAR
+            </button>
           </form>
         </div>
       </main>
