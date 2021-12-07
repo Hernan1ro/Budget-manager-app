c2aaf5f1143dc27e6827af2de7da631439ac32c8
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-07 18:41:21 -0500

    Objective section created

--- src/pages/ObjectiveMonth/index.jsx
+++ src/pages/ObjectiveMonth/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Menu from "../../components/Menu";
 import WhiteBackground from "../../layout/WhiteBackground";
+import "./styles.css";
 
 const ObjectiveMonth = () => {
   return (
@@ -10,20 +11,33 @@ const ObjectiveMonth = () => {
         <div className="objective-container">
           <div className="title-container">
             <h1 className="objective-container__title">
-              Objetivos para el Mes
+              OBJETIVOS PARA EL MES
             </h1>
           </div>
-          <form action="" className="objective-form">
-            <label htmlFor="month-budget">Presupuesto Mensual</label>
-            <input id="month-budget" type="number" />
-            <label htmlFor="month-expenses">Gasto mensual actual</label>
-            <input id="month-expenses" type="number" />
-            <label htmlFor="average-savings">Ahorro estimado</label>
-            <input id="average-savings" type="number" />
-            <button id="form-button" type="">
-              Guardar
-            </button>
-          </form>
+          <div className="objective-keys-container">
+            <form action="" className="objective-form">
+              <div className="label-container">
+                <label htmlFor="month-budget">Presupuesto Mensual</label>
+                <input id="month-budget" type="number" />
+              </div>
+              <div className="label-container">
+                <label htmlFor="month-expenses">Gasto mensual actual</label>
+                <input id="month-expenses" type="number" />
+              </div>
+              <div className="label-container">
+                <label htmlFor="average-savings">Ahorro estimado</label>
+                <input id="average-savings" type="number" />
+              </div>
+              <button id="form-button" type="">
+                Guardar
+              </button>
+            </form>
+            <div className="alert-container">
+              <div className="alert-message">
+                {/* <h3>No hay novedades</h3> */}
+              </div>
+            </div>
+          </div>
         </div>
       </WhiteBackground>
     </>
