c9860f0d6840d732c24cd8801620aa553ad8705b
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-07 00:19:50 -0500

    New record component html

--- src/pages/Income/index.jsx
+++ src/pages/Income/index.jsx
@@ -7,7 +7,63 @@ const Income = () => {
     <>
       <Menu />
       <WhiteBackground>
-        <div>Income</div>
+        <div className="income-container">
+          <div className="new-record">
+            <h2 className="new-record__title">Nuevo Ingreso</h2>
+            <p className="new-record__subtitle">
+              Debe tener un valor mayor que 0
+            </p>
+            <form action="" className="new-record__form">
+              <div className="input-container">
+                <img src="" alt="" />
+                <input
+                  name="value"
+                  type="number"
+                  className="input-container__value"
+                  placeholder="Valor"
+                />
+              </div>
+              <div className="input-container">
+                <img src="" alt="" />
+                <input
+                  name="date"
+                  type="date"
+                  className="input-container__date"
+                  placeholder="Fecha"
+                />
+              </div>
+              <div className="input-container">
+                <img src="" alt="" />
+                <input
+                  name="description"
+                  type="text"
+                  className="input-container__description"
+                  placeholder="Descripción"
+                />
+              </div>
+              <div className="input-container">
+                <img src="" alt="" />
+                <input
+                  name="category"
+                  type="text"
+                  className="input-container__category"
+                  placeholder="Categoria"
+                />
+              </div>
+              <button type="submit">Registrar nuevo ingreso</button>
+            </form>
+          </div>
+          <div className="records-list">
+            <div className="records-list__month">
+              <div className="month">
+                <img src="" alt="arrow-icon" />
+                <div className="month__element">Noviembre 2021</div>
+                <img src="" alt="arrow-icon" />
+              </div>
+            </div>
+            <div className="records-list__data"></div>
+          </div>
+        </div>
       </WhiteBackground>
     </>
   );
