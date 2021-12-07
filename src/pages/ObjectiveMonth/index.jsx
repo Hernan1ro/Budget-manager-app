837c81be871352cf117fb0d64664e43978229437
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-07 17:25:15 -0500

    Objectivo month html page

--- src/pages/ObjectiveMonth/index.jsx
+++ src/pages/ObjectiveMonth/index.jsx
@@ -7,7 +7,24 @@ const ObjectiveMonth = () => {
     <>
       <Menu />
       <WhiteBackground>
-        <div>Objective month</div>
+        <div className="objective-container">
+          <div className="title-container">
+            <h1 className="objective-container__title">
+              Objetivos para el Mes
+            </h1>
+          </div>
+          <form action="" className="objective-form">
+            <label htmlFor="month-budget">Presupuesto Mensual</label>
+            <input id="month-budget" type="number" />
+            <label htmlFor="month-expenses">Gasto mensual actual</label>
+            <input id="month-expenses" type="number" />
+            <label htmlFor="average-savings">Ahorro estimado</label>
+            <input id="average-savings" type="number" />
+            <button id="form-button" type="">
+              Guardar
+            </button>
+          </form>
+        </div>
       </WhiteBackground>
     </>
   );
