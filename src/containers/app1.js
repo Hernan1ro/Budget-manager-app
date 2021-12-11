1aa85c6c1e91d23dcf463d1931ffaf6e00ad031d
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-11 09:16:39 -0500

    adding dinamic data to the chart

--- src/containers/app1.js
+++ src/containers/app1.js
@@ -7,26 +7,41 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
+import { useSelector } from "react-redux";
 
 const data = [
   {
     name: "Gastos Hormiga",
-    gastosHormiga: 120,
+    gastosHormiga: 0,
     keystroke: 1000,
   },
   {
     name: "Gastos fijos",
-    costoFijos: 300,
+    costoFijos: 0,
     keystroke: 1000,
   },
   {
     name: "Ingresos",
-    ingresos: 400,
+    ingresos: 0,
     keystroke: 1000,
   },
 ];
 
 function App() {
+  const antExpenses = useSelector((state) => state.antExpense);
+  const income = useSelector((state) => state.income);
+  const fixedExpenses = useSelector((state) => state.fixedExpense);
+
+  const totalAntExpense = antExpenses.reduce((sum, item) => sum + item.value);
+  const totalFixedExpense = fixedExpenses.reduce(
+    (sum, item) => sum + item.value
+  );
+  const totalIncome = income.reduce((sum, item) => sum + item.value);
+
+  data[0].ingresos = totalAntExpense;
+  data[1].ingresos = totalFixedExpense;
+  data[2].ingresos = totalIncome;
+
   return (
     <ResponsiveContainer width="90%">
       <BarChart width={500} height={300} data={data}>
